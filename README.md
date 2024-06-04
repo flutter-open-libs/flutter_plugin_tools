a3447043d4967d9ea649627859f1d18118921d54
Sing <>
2024-06-04 12:02:15 +0800

    拓展通用列表项，使用更加灵活

--- README.md
+++ README.md
@@ -50,19 +50,37 @@ dependencies:
   import 'package:sing_plugin_tools/widgets/sing_cell_item.dart';
 
   SingCellItem(
-    height: 56.0,
-    padding: const EdgeInsets.symmetric(horizontal: 16.0,vertical: 0.0),
-    assetPath: 'assets/images/ic_feedback.png',
-    iconSize: const Size(28.0,28.0),
-    gap: 12.0,
-    text: '我是Item',
-    textColor: const Color(0xff131732),
-    textSize: 16.0,
-    decoration: const BoxDecoration(border: Border(bottom: BorderSide(color: Colors.grey, width: 0.2))),
-    onTap: ()=> {},
-    showNext: true,
-    separator: Container(width: 200.0,height: 1.0,color: Colors.grey),
-    nextIcon: const Icon(Icons.arrow_forward_ios,size: 16.0,color:Colors.grey),
+    backgroundColor: Colors.white, // 背景颜色，默认透明
+    height: 56.0, // item 的高度
+    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+    // assetPath: 'assets/images/ic_feedback.png', // 前面 icon 的 asset 路径，为空则不展示
+    iconSize: const Size(28.0,28.0), // 前面 icon 的大小，assetPath 为空则不展示
+    gap: 12.0, // icon 和标题的间距
+    title: '我是Item', // 标题
+    titleTextStyle: const TextStyle(color: Color(0xFF131732), fontSize: 16.0), // 标题样式
+    decoration: const BoxDecoration(border: Border(bottom: BorderSide(color: Colors.grey, width: 0.2))), // 边框的样式，默认底部一条线
+    onTap: ()=> print('点击主标题'), // item 的点击事件
+    // separator: Container(width: 200.0,height: 1.0,color: Colors.grey), // 自定义的装饰，正常是一根线，在 decoration 满足不了的情况下自定义，比如项左右有边距。当然其他 widget 也可以
+    showNext: true, // 是否显示右侧箭头
+    nextIcon: const Icon(Icons.arrow_forward_ios,size: 16.0,color:Colors.grey), // 右侧的箭头，可以是任何 Widget
+
+    showSub: false, // 是否显示副标题，副标题是一个 TextField
+    focusNode: FocusNode(), // 控制副标题的副控件 KeyboardListener
+    controller: textController, // TextField 的 controller ，可以设置文字，取值也通过他
+    subTitleTextStyle : const TextStyle(color: Color(0xFF131732), fontSize: 14.0), // 副标题样式
+    subTitleHint: '请输入副标题', // 副标题的提示语
+    subTitleHintTextStyle : const TextStyle(color: Colors.grey, fontSize: 14.0),// 副标题的提示语的样式
+    subTitleContentPadding : const EdgeInsets.symmetric(vertical: 12.0), // 副标题的 ContentPadding
+    readOnly:false, // 副标题 TextField 的是否能编辑
+    onSubTap: ()=> print('点击副标题'), // 副标题点击事件
+    customWidget: Container( // 自定义的 Widget ，在副标题之后，箭头之前，自己控制间距
+      decoration: const BoxDecoration(
+          color: Color(0xffE9F7FE),
+          borderRadius: BorderRadius.all(Radius.circular(8.0))
+      ),
+      margin: const EdgeInsets.symmetric(horizontal: 5.0),
+      child: const Text('  自定义  ',style: TextStyle(color: Colors.grey,fontSize: 12.0))
+    )
   ),
   ```
 
--- lib/widgets/sing_cell_item.dart
+++ lib/widgets/sing_cell_item.dart
@@ -11,14 +11,25 @@ class SingCellItem extends StatelessWidget {
     this.assetPath = '', // 如果为空这不显示图标
     this.iconSize = const Size(28.0,28.0),
     this.gap = 12.0,
-    this.text = 'CellItem',
-    this.textColor = const Color(0xff131732),
-    this.textSize = 16.0,
+    this.title = 'CellItem',
+    this.titleTextStyle,
     this.decoration,
     this.onTap,
     this.separator,
     this.showNext = false,
     this.nextIcon,
+
+    this.focusNode,
+    this.controller,
+    this.subTitleTextStyle,
+    this.subTitleHint='',
+    this.subTitleHintTextStyle,
+    this.subTitleContentPadding,
+    this.readOnly = true,
+    this.onSubTap,
+    this.showSub = true,
+
+    this.customWidget,
   });
 
   final Color? backgroundColor; /// 背景颜色
@@ -27,19 +38,30 @@ class SingCellItem extends StatelessWidget {
   final String assetPath; /// asset图标的路径
   final Size iconSize; /// 图标的大小
   final double gap; /// 图标和文字的间距
-  final String text; /// 文字
-  final Color textColor; /// 文字颜色
-  final double textSize; /// 文字大小
+  final String title; /// 文字
+  final TextStyle? titleTextStyle; /// 文字样式
   final BoxDecoration? decoration; /// item的装饰
   final GestureTapCallback? onTap; /// item的点击回调
   final Widget? separator; /// 自定义的装饰，正常是一根线，在 decoration 满足不了的情况下自定义，比如项左右有边距。当然其他 widget 也可以
   final bool showNext; /// 是否展示右侧的箭头
   final Widget? nextIcon; /// 自定义的右侧icon
 
+  final FocusNode? focusNode;
+  final TextEditingController? controller;
+  final TextStyle? subTitleTextStyle; /// 副标题文字样式
+  final String subTitleHint; /// 副标题提示文字
+  final TextStyle? subTitleHintTextStyle; /// 副标题提示样式
+  final EdgeInsets? subTitleContentPadding;
+  final bool readOnly; /// 副标题是否不能输入
+  final GestureTapCallback? onSubTap; /// 副标题点击
+  final bool showSub; /// 是否显示副标题
+
+  final Widget? customWidget; /// 自定义的右侧 Widget，在副标题后，箭头前，自己控制边距
+
   @override
   Widget build(BuildContext context) {
     return  Container(
-      color: backgroundColor ?? Colors.white,
+      color: backgroundColor ?? Colors.transparent,
       child: Column(
         children: [
           InkWell(
@@ -53,11 +75,42 @@ class SingCellItem extends StatelessWidget {
                   Image.asset(assetPath, width: iconSize.width, height: iconSize.height),
                 if (assetPath.isNotEmpty)
                   SizedBox(width: gap),
-                Text(text,style: TextStyle(color: textColor, fontSize: textSize)),
+                Text(title,style: titleTextStyle ?? const TextStyle(color: Color(0xFF131732), fontSize: 16.0)),
+                SizedBox(width: gap),
+                if(showSub)
+                  Expanded(
+                    child: KeyboardListener(
+                      focusNode: focusNode ?? FocusNode(),
+                      child: TextField(
+                        onTap: onSubTap ?? () => debugPrint('SingCellItem sub clicked'),
+                        readOnly : readOnly,
+                        controller: controller ?? TextEditingController(),
+                        textAlign: TextAlign.end,
+                        style: subTitleTextStyle ?? const TextStyle(color: Color(0xFF131732), fontSize: 14.0),
+                        decoration: InputDecoration(
+                          prefixText: "  ",
+                          isDense: true,
+                          contentPadding: subTitleContentPadding ?? const EdgeInsets.symmetric(vertical: 12.0),
+                          counterText: "", // 去掉底部计数
+                          enabledBorder: const UnderlineInputBorder(borderSide: BorderSide.none), //无焦点时状态
+                          focusedBorder: const UnderlineInputBorder(borderSide: BorderSide.none), //有焦点时状态
+                          hintText: subTitleHint,
+                          hintStyle: subTitleHintTextStyle ?? const TextStyle(color: Colors.grey, fontSize: 14.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                if(!showSub)
+                  Expanded(
+                    child: Container(),
+                  ),
+                if (customWidget != null)
+                  customWidget!,
                 if (showNext)
-                  Expanded(child: Container()),
-                if (showNext)
-                  nextIcon ?? const Icon(Icons.arrow_forward_ios,size: 16.0,color:Colors.grey),
+                  InkWell(
+                    onTap: onSubTap ?? onTap ?? () => debugPrint('SingCellItem sub arrow'),
+                    child: nextIcon ?? const Icon(Icons.arrow_forward_ios,size: 16.0,color:Colors.grey),
+                  ),
               ]),
             ),
           ),
